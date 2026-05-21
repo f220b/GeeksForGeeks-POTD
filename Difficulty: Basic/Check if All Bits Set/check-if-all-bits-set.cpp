c733f585725a5c8c7a31f9bbc0ed918a49f9fb7b
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-05-21 15:00:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Check if All Bits Set/check-if-all-bits-set.cpp
@@ -0,0 +1,14 @@
+class Solution {
+	public:
+	bool isBitSet(int n) {
+		// code here
+		if (n == 0)
+			return false;
+		while (n > 0) {
+			if (n%2 == 0)
+				return false;
+			n /= 2;
+		}
+		return true;
+	}
+};
