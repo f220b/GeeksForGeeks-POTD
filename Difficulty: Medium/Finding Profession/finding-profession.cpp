cc7cb7b36b560200510860acf33a666f8e175a5b
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-06-07 20:31:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Finding Profession/finding-profession.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    string profession(int level, int pos) {
+        int flips = __builtin_popcount(pos - 1);
+        return (flips % 2 == 0) ? "Engineer" : "Doctor";
+    }
+};
