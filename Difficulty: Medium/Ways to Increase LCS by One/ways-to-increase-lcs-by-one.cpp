4bcd864e95391b6149ce513345a07f5e8d3ad2f9
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-07-03 12:26:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Ways to Increase LCS by One/ways-to-increase-lcs-by-one.cpp
+++ Difficulty: Medium/Ways to Increase LCS by One/ways-to-increase-lcs-by-one.cpp
@@ -1,15 +1,16 @@
 class Solution {
 	public:
+	using vi = vector<int>;
 	int helper(string &s1, string &s2) {
 		int n1 = s1.length();
 		int n2 = s2.length();
 		
-		vector<vector<int>> position(26);
+		vector<vi> position(26);
 		for (int i = 1; i <= n2; i++)
 			position[s2[i - 1] - 'a'].push_back(i);
 		
-		vector<vector<int>> lcsl(n1 + 2, vector<int>(n2 + 2, 0));
-		vector<vector<int>> lcsr(n1 + 2, vector<int>(n2 + 2, 0));
+		vector<vi> lcsl(n1 + 2, vi(n2 + 2, 0));
+		vector<vi> lcsr(n1 + 2, vi(n2 + 2, 0));
 		for (int i = 1; i <= n1; i++) {
 			for (int j = 1; j <= n2; j++) {
 				if (s1[i - 1] == s2[j - 1])
@@ -32,7 +33,7 @@ class Solution {
 		int baseLCS = lcsl[n1][n2];
 		for (int i = 0; i <= n1; i++) {
 			for (char c = 'a'; c <= 'z'; c++) {
-				vector<int> &posList = position[c - 'a'];
+				vi &posList = position[c - 'a'];
 				for (int j = 0; j < (int)posList.size(); j++) {
 					int p = posList[j];
 					
